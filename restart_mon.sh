5ea0a18ceacd9c9418c1da5093b1c3619ff68f92
Oleg Klimenko <o.klimenko@2gis.ru>
2015-01-15 14:02:35 +0500

    Update restart_mon.sh
    Remove kiosk mode for good

--- restart_mon.sh
+++ restart_mon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ps ax | grep chrome | awk '{print $1}' | xargs kill
 sleep 1
-DISPLAY=:0 google-chrome --test-type --allow-files-access-from-files --disable-web-security --kiosk "/home/online/monitoring/monitor.html" &
+DISPLAY=:0 google-chrome --test-type --allow-files-access-from-files --disable-web-security "/home/online/monitoring/monitor.html" &
 
